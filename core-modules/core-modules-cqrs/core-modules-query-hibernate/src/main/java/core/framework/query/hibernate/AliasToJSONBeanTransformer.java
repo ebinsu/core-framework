bf1077306977e332b44b0d569141c3406f9b5e75
ebin <ebinsu@chancetop.com>
2023-03-22 15:18:52 +0800

    fix query

--- core-modules/core-modules-cqrs/core-modules-query-hibernate/src/main/java/core/framework/query/hibernate/AliasToJSONBeanTransformer.java
+++ core-modules/core-modules-cqrs/core-modules-query-hibernate/src/main/java/core/framework/query/hibernate/AliasToJSONBeanTransformer.java
@@ -1,16 +1,19 @@
 package core.framework.query.hibernate;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.query.TupleTransformer;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author ebin
  */
 public class AliasToJSONBeanTransformer implements TupleTransformer<Object> {
+    private static final String CAMEL_SPLIT = "_";
     private final Class<?> resultClass;
 
     public AliasToJSONBeanTransformer(Class<?> resultClass) {
@@ -22,22 +25,20 @@ public class AliasToJSONBeanTransformer implements TupleTransformer<Object> {
         Object result;
         try {
             result = resultClass.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                 NoSuchMethodException e) {
+        } catch (InstantiationException
+                 | IllegalAccessException
+                 | InvocationTargetException
+                 | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
         for (int i = 0; i < tuple.length; i++) {
             String alias = aliases[i];
             if (alias != null) {
-                Field declaredField;
                 try {
-                    declaredField = resultClass.getDeclaredField(alias.toLowerCase(Locale.getDefault()));
-                } catch (NoSuchFieldException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
+                    Field declaredField = resultClass.getDeclaredField(camelCase(alias));
                     declaredField.set(result, tuple[i]);
-                } catch (IllegalAccessException e) {
+                } catch (NoSuchFieldException
+                         | IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
             }
@@ -45,37 +46,18 @@ public class AliasToJSONBeanTransformer implements TupleTransformer<Object> {
         return result;
     }
 
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        A a = A.class.getDeclaredConstructor().newInstance();
-        Arrays.stream(A.class.getDeclaredFields()).forEach(f -> {
-            try {
-                f.set(a, "a");
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        System.out.println(a.getA());
-        System.out.println(a.getB());
-    }
-
-    public static class A {
-        private String a;
-        public String b;
-
-        public String getA() {
-            return a;
+    private String camelCase(String input) {
+        String output = input.toLowerCase(Locale.getDefault());
+        if (output.contains(CAMEL_SPLIT)) {
+            String[] split = output.split(CAMEL_SPLIT);
+            output = IntStream.range(0, split.length).mapToObj(index -> {
+                if (index > 0) {
+                    return StringUtils.capitalize(split[index]);
+                } else {
+                    return split[index];
+                }
+            }).collect(Collectors.joining());
         }
-
-        public void setA(String a) {
-            this.a = a;
-        }
-
-        public String getB() {
-            return b;
-        }
-
-        public void setB(String b) {
-            this.b = b;
-        }
+        return output;
     }
 }
