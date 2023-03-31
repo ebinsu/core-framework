f6c34c1fd68526f42b8bda16d1fb6a81a505e172
ebin <ebinSu@xm.remarkablefoods.com>
2023-03-31 16:58:10 +0800

    add actuator support

---
+++ core-modules/core-modules-web/src/main/java/core/framework/web/configuration/EnvironmentApplicationListener.java
@@ -0,0 +1,21 @@
+package core.framework.web.configuration;
+
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+/**
+ * @author ebin
+ */
+@Component
+public class EnvironmentApplicationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+        Properties props = new Properties();
+        props.put("management.endpoint.health.probes.enabled", false);
+        event.getEnvironment().getPropertySources().addFirst(new PropertiesPropertySource("actuator-override-properties", props));
+    }
+}
