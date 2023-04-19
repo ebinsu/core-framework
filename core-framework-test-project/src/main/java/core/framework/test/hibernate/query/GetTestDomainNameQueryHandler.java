80c099072d794db6418b6a32845afbe0a09400fd
ebin <ebinsu@chancetop.com>
2023-04-19 15:40:41 +0800

    refactor cqrs query module

--- core-framework-test-project/src/main/java/core/framework/test/hibernate/query/GetTestDomainNameQueryHandler.java
+++ core-framework-test-project/src/main/java/core/framework/test/hibernate/query/GetTestDomainNameQueryHandler.java
@@ -1,19 +1,22 @@
 package core.framework.test.hibernate.query;
 
 import core.framework.query.annotation.QueryHandler;
+import core.framework.query.support.NameQueryService;
 import core.framework.query.support.namequery.NameQueryParamImpl;
-import core.framework.query.support.namequery.NameQueryServiceAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * @author ebin
  */
 @Service
-public class GetTestDomainNameQueryHandler extends NameQueryServiceAdapter {
+public class GetTestDomainNameQueryHandler {
+    @Autowired
+    private NameQueryService nameQueryService;
 
     @QueryHandler
     public TestDomainDTO handle(GetTestDomainQuery query) {
         NameQueryParamImpl<TestDomainDTO> nameQueryParamImpl = new NameQueryParamImpl<>("TestDomain.get", TestDomainDTO.class);
-        return this.get(nameQueryParamImpl).orElseThrow();
+        return nameQueryService.get(nameQueryParamImpl).orElseThrow();
     }
 }
--- core-framework-test-project/src/main/java/core/framework/test/hibernate/query/ListTestDomainNameQueryHandler.java
+++ core-framework-test-project/src/main/java/core/framework/test/hibernate/query/ListTestDomainNameQueryHandler.java
@@ -1,8 +1,9 @@
 package core.framework.test.hibernate.query;
 
 import core.framework.query.annotation.QueryHandler;
+import core.framework.query.support.NameQueryService;
 import core.framework.query.support.namequery.NameQueryParamImpl;
-import core.framework.query.support.namequery.NameQueryServiceAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +13,14 @@ import java.util.List;
  * @author ebin
  */
 @Service
-public class ListTestDomainNameQueryHandler extends NameQueryServiceAdapter {
+public class ListTestDomainNameQueryHandler {
+    @Autowired
+    private NameQueryService nameQueryService;
+
     @Transactional
     @QueryHandler
     public List<TestDomainDTO> handle(ListTestDomainQuery query) {
         NameQueryParamImpl<TestDomainDTO> nameQueryParamImpl = new NameQueryParamImpl<>("TestDomain.get", TestDomainDTO.class);
-        return this.select(nameQueryParamImpl);
+        return nameQueryService.select(nameQueryParamImpl);
     }
 }
