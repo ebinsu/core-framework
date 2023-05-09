baba5f0386396804fbcce0316488d0acb48f311e
ebin <ebinsu@chancetop.com>
2023-05-09 11:43:56 +0800

    remove tool module

--- tools/extending-checkstyle/src/test/resources/test1/domain/DomainLayerClassB.java
+++
@@ -1,9 +0,0 @@
-package test1.domain;
-
-/**
- * @author ebin
- */
-@AggregateRoot
-public class DomainLayerClassB {
-    public String id;
-}
--- tools/extending-checkstyle/src/test/resources/test2/domain/DomainLayerClassC.java
+++
@@ -1,9 +0,0 @@
-package test2.domain;
-
-/**
- * @author ebin
- */
-@AggregateRoot
-public class DomainLayerClassC {
-    public String id;
-}
