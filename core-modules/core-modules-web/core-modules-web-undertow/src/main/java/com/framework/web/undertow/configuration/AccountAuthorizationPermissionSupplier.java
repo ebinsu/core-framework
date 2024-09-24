ba30cae2566da4e53c00b0481ee65d0e0a5142d5
ebin <ebinsu@chancetop.com>
2024-09-24 18:02:43 +0800

    fixbug

--- core-modules/core-modules-web/core-modules-web-undertow/src/main/java/com/framework/web/undertow/configuration/AccountAuthorizationPermissionSupplier.java
+++ core-modules/core-modules-web/core-modules-web-undertow/src/main/java/com/framework/web/undertow/configuration/AccountAuthorizationPermissionSupplier.java
@@ -3,6 +3,7 @@ package com.framework.web.undertow.configuration;
 import core.framework.web.common.security.filter.AuthorizationPermissionSupplier;
 import io.undertow.security.api.AuthenticatedSessionManager;
 import io.undertow.servlet.handlers.security.CachedAuthenticatedSessionHandler;
+import io.undertow.servlet.spec.HttpSessionImpl;
 import jakarta.servlet.http.HttpSession;
 
 import java.util.Set;
@@ -13,7 +14,11 @@ import java.util.Set;
 public class AccountAuthorizationPermissionSupplier implements AuthorizationPermissionSupplier {
     @Override
     public Set<String> getPermissions(HttpSession session) {
-        AuthenticatedSessionManager.AuthenticatedSession authenticatedSession = (AuthenticatedSessionManager.AuthenticatedSession) session.getAttribute(CachedAuthenticatedSessionHandler.ATTRIBUTE_NAME);
-        return authenticatedSession.getAccount().getRoles();
+        if (session instanceof HttpSessionImpl impl) {
+            AuthenticatedSessionManager.AuthenticatedSession authenticatedSession =
+                (AuthenticatedSessionManager.AuthenticatedSession) impl.getSession().getAttribute(CachedAuthenticatedSessionHandler.ATTRIBUTE_NAME);
+            return authenticatedSession.getAccount().getRoles();
+        }
+        return Set.of();
     }
 }
