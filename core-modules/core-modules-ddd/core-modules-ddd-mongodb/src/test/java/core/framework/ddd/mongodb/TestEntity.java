c6bfd93c2da6842e70079597029256d40a87c020
ebin <ebinsu@chancetop.com>
2024-10-08 14:39:55 +0800

    add mongo ddd module

--- core-modules/core-modules-ddd/core-modules-ddd-mongodb/src/test/java/core/framework/ddd/mongodb/TestEntity.java
+++ core-modules/core-modules-ddd/core-modules-ddd-mongodb/src/test/java/core/framework/ddd/mongodb/TestEntity.java
@@ -3,6 +3,10 @@ package core.framework.ddd.mongodb;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * @author ebin
  */
@@ -13,26 +17,46 @@ public class TestEntity extends AbstractAggregateRoot {
 
     private String name;
 
+    private LocalDateTime localDateTime;
+
+    private LocalDate localDate;
+
+    private BigDecimal bigDecimal;
+
+    private Integer integer;
+
     public TestEntity() {
     }
 
     public TestEntity(String name) {
         this.name = name;
+        this.localDateTime = LocalDateTime.now();
+        this.localDate = LocalDate.now();
+        this.bigDecimal = BigDecimal.ONE;
+        this.integer = 100;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public BigDecimal getBigDecimal() {
+        return bigDecimal;
+    }
+
+    public Integer getInteger() {
+        return integer;
     }
 }
