8e307841cbd8071f27dae2a486f54cff89418b69
ebin <ebinsu@chancetop.com>
2025-05-26 14:40:39 +0800

    wip add kernel module -- logs

--- core-modules/core-modules-kernel/src/main/java/core/framework/kernel/log/Logs.java
+++ core-modules/core-modules-kernel/src/main/java/core/framework/kernel/log/Logs.java
@@ -1,4 +1,4 @@
-package core.framework.kernel.log.marker;
+package core.framework.kernel.log;
 
 import org.slf4j.MDC;
 
--- core-modules/core-modules-web/core-modules-web-common/src/main/java/core/framework/web/common/configuration/DefaultWebMvcConfigurer.java
+++ core-modules/core-modules-web/core-modules-web-common/src/main/java/core/framework/web/common/configuration/DefaultWebMvcConfigurer.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
